96759882368be11e35fe546c9bfc0ae96da104c2
jiankeabcd <jiankeabcd@users.noreply.github.com>
2024-08-13 21:44:53 +0800

    anime.js

--- script.js
+++ script.js
@@ -6,61 +6,60 @@ anime({
     opacity: [0, 1],
     duration: 1500,
     easing: 'easeOutExpo'
-  });
-  
-  // Animate About Section
-  anime({
+});
+
+// Animate About Section
+anime({
     targets: '.about-section .intro h1',
     translateY: [-50, 0],
     opacity: [0, 1],
     duration: 1500,
     delay: 500,
     easing: 'easeOutExpo'
-  });
-  
-  anime({
+});
+
+anime({
     targets: '.about-section .intro p',
     translateY: [50, 0],
     opacity: [0, 1],
     duration: 1500,
     delay: 1000,
     easing: 'easeOutExpo'
-  });
-  
-  anime({
+});
+
+anime({
     targets: '.about-section .portrait img',
     scale: [0.8, 1],
     opacity: [0, 1],
     duration: 1500,
     delay: 1500,
     easing: 'easeOutExpo'
-  });
-  
-  // Animate Work Section
-  anime({
+});
+
+// Animate Work Section
+anime({
     targets: '.work-section h2',
     translateY: [-50, 0],
     opacity: [0, 1],
     duration: 1500,
     delay: 500,
     easing: 'easeOutExpo'
-  });
-  
-  anime({
+});
+
+anime({
     targets: '.work-item',
     scale: [0.8, 1],
     opacity: [0, 1],
     duration: 1000,
-    delay: anime.stagger(200, {start: 1000}),
+    delay: anime.stagger(200, { start: 1000 }),
     easing: 'easeOutExpo'
-  });
-  
-  // Animate Contact Section
-  anime({
+});
+
+// Animate Contact Section
+anime({
     targets: '.contact-section',
     opacity: [0, 1],
     duration: 2000,
     delay: 2000,
     easing: 'easeOutExpo'
-  });
-  
+});
